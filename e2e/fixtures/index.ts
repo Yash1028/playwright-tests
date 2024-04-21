47b21cb6b0fcadd26fc289d9958d9452638e2b99
Yash1028 <yash11125@gmail.com>
2024-04-21 19:17:53 +0530

    Added tests for Register page and created Page Object Models and fixtures for login

--- e2e/fixtures/index.ts
+++ e2e/fixtures/index.ts
@@ -4,11 +4,11 @@ import {test as base} from "@playwright/test"
 import LoginPage from "../poms/login"
 
 interface  ExtendedFixtures {
-    loginpage: LoginPage;
+    loginPage: LoginPage;
 }
 
 export const test = base.extend<ExtendedFixtures>({
-    loginpage: async({page},use)=>{
+    loginPage: async({page},use)=>{
         const loginPage = new LoginPage(page);
         await use(loginPage);
     },
--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,14 +1,12 @@
 // login.spec.ts
 
-import { test, expect } from "@playwright/test";
-import LoginPage from "../poms/login";
-
+import { test } from "../fixtures"
 
 test.describe("Login page", () => {
-  test("should login with the correct credentials", async ({ page }) => {
-    const login = new LoginPage(page);
+  test("should login with the correct credentials", async ({ page,loginPage }) => {
+    // const login = new loginpage(page);
     await page.goto("http://localhost:3000");
-    await login.loginAndVerifyUser({
+    await loginPage.loginAndVerifyUser({
       email: "oliver@example.com",
       password: "welcome",
       username: "Oliver Smith",
